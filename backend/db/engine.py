5939b2441ea8ae6a3259d1e48de902cface5d5e5
FeathersRe <liaoyuzhou123@gmail.com>
2026-03-24 02:08:00 +0800

    Add engine.py to initialise SQLalchemy engine and session

---
+++ backend/db/engine.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from backend.config import settings
+
+'''
+Sets up SQLalchemy for relational DB
+'''
+
+# Create SQLalchemy engine and session
+engine = create_engine(settings.DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(bind=engine)
+
+# Create base class for ORM models
+class Base(DeclarativeBase):
+    pass
+# Create session generator
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
